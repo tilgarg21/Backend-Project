fdde9d41d87d67ad167bcfe7c6b987b6ccfcdf18
Tilak Garg <tilgarg21@gmail.com>
2025-09-22 16:51:44 +0530

    Added watch history retreival functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -349,6 +349,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     "User channel fetched successfully"));
 });
 
+const getWatchHistory = asyncHandler(async (req, res)=> {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+      from: "videos",
+      localField: "watchHistory",
+      foreignField: "_id",
+      as: "watchHistory" ,
+      pipeline: [
+        {
+          $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner",
+            pipeline: [
+              {
+                $project: {
+                  fullName: 1,
+                  username: 1,
+                  avatar: 1
+                }
+              }
+            ]
+          }
+        },
+        {
+          $addFields: {
+            owner: {
+              $first: "$owner"
+            }
+          }
+        }
+      ]
+    }
+  }
+  ])
+
+  return res.status(200)
+.json(new apiResponse(200,
+  user[0].watchHistory,
+  "Watch history fetched successfully"
+))
+})
+
+
+
 export {
   registerUser,
   loginUser,
@@ -360,4 +412,6 @@ export {
   updateUserAvatar,
   updateCoverImage,
   getUserChannelProfile,
+  getWatchHistory
+  
 };
